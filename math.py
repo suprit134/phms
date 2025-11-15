e71733237e3ad1e633bb3674b915a1f2258416ce
suprit134 <smavanur134@gmail.com>
2025-11-15 08:37:57 +0530

    Implement basic arithmetic operations in math.py

---
+++ math.py
@@ -0,0 +1,7 @@
+try:
+  a=float("Enter first number:"))
+  b=float("Input("Enter second number:"))
+  print(f"Addition:{a+b}")
+  print(f"Subtraction:{a-b}")
+  except ValueError:
+  print("Please enter valid number.")
